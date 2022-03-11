65cbf0170576733d5614060fa5288e36725a5237
Isabella Chagas <bellamarchagas@gmail.com>
2022-03-11 18:36:39 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import ProductsDetails from './pages/ProductsDetails';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <BrowserRouter>
           <Route exact path="/" component={ Home } />
           <Route exact path="/cart" component={ Cart } />
+          <Route exact path="/produtcs/details" component={ ProductsDetails } />
         </BrowserRouter>
       </div>
     );
